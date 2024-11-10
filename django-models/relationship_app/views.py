6dcbc900c831baebb83bc523fb2d9bd2d0b116e4
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-10 22:02:35 +0200

    back -1

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,24 +1,29 @@
 # relationship_app/views.py
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library  
+from .models import Library
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.detail import DetailView
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views import View
-from .models import Book, Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})  
 
 # Class-based view to display library details
-class LibraryDetailView(DetailView):
+class LibraryDetailView(DetailView):  
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  
     context_object_name = 'library'
 
-# RegisterView for user registration
+# class registration view
 class RegisterView(View):
     def get(self, request):
         form = UserCreationForm()
@@ -30,15 +35,10 @@ class RegisterView(View):
             form.save()
             return redirect('login')
         return render(request, 'relationship_app/register.html', {'form': form})
-
-# Custom login view
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-# Custom logout view
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-    template_name = 'relationship_app/login.html'
+    
+    # login view
+    class CustomLoginView(LoginView):
+        template_name = 'relationship_app/login.html'
 
     # logout view
     class CustomLogoutView(LogoutView):
