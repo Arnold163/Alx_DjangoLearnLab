25fd3626e6a5de9b91b1e6657c1c28ae6ccd5c7a
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-11 15:34:53 +0200

    custom permission

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,6 +12,10 @@ from django.views import View
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
 
 # Function-based view to list all books
 def list_books(request):
@@ -74,3 +78,33 @@ class RegisterView(View):
         return render(request, 'relationship_app/access_denied.html')
 
 
+    @permission_required('relationship_app.can_add_book', raise_exception=True)
+    def add_book(request):
+        if request.method == 'POST':
+            form = BookForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('book_list')  # redirect to a list of books or another appropriate page
+        else:
+            form = BookForm()
+        return render(request, 'relationship_app/add_book.html', {'form': form})
+
+    @permission_required('relationship_app.can_change_book', raise_exception=True)
+    def edit_book(request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        if request.method == 'POST':
+            form = BookForm(request.POST, instance=book)
+            if form.is_valid():
+                form.save()
+                return redirect('book_list')
+        else:
+            form = BookForm(instance=book)
+        return render(request, 'relationship_app/edit_book.html', {'form': form})
+
+    @permission_required('relationship_app.can_delete_book', raise_exception=True)
+    def delete_book(request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        if request.method == 'POST':
+            book.delete()
+            return redirect('book_list')
+        return render(request, 'relationship_app/delete_book.html', {'book': book})
