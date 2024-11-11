91a9e200ed47516d16595a6eb50e8b21acff5e38
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-11 15:16:07 +0200

    admn view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -47,29 +47,30 @@ class RegisterView(View):
 
     # Role based views 
     def is_admin(user):
-        return user.userprofile.role == 'admin'
+        return hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
     
     def is_library(user):
-        return user.userprofile.role == 'library'
+        return hasattr(user, 'userprofile') and user.userprofile.role == 'library'
     
     def is_member(user):
-        return user.userprofile.role == 'member'
+        return hasattr(user, 'userprofile') and user.userprofile.role == 'member'
     
-    # admin view
-    @user_passes_test(is_admin)
+   # Admin view, accessible only to users with the 'admin' role
+    @user_passes_test(is_admin, login_url='/access-denied/')
     def admin_view(request):
-        return render(request, 'relationship_app/admin.html')
-    
-    # library view
-    @user_passes_test(is_library)
+     return render(request, 'relationship_app/admin.html')
+
+# Library view, accessible only to users with the 'library' role
+    @user_passes_test(is_library, login_url='/access-denied/')
     def library_view(request):
         return render(request, 'relationship_app/library.html')
-    
-    # member view
-    @user_passes_test(is_member)
+
+# Member view, accessible only to users with the 'member' role
+    @user_passes_test(is_member, login_url='/access-denied/')
     def member_view(request):
         return render(request, 'relationship_app/member.html')
     
-    
+    def access_denied(request):
+        return render(request, 'relationship_app/access_denied.html')
 
 
