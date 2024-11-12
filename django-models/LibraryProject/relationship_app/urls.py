61cbcbb71051925216a6b25072fb3de1699a90a8
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-13 01:22:22 +0200

    changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('access-denied/', access_denied, name='access_denied'),
 
 
-     path('add_book/', views.add_book, name='add_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
     
