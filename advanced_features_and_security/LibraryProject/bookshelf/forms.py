8965fd06b542fa7abe88515934a9ef47cf185f0d
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-16 02:08:14 +0200

    example form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=True)
+
+    def clean_example_field(self):
+        data = self.cleaned_data['example_field']
+        
+        # Example of sanitization or additional validation
+        if "<script>" in data:
+            raise forms.ValidationError("Invalid input - scripts are not allowed.")
+        
+        return data
