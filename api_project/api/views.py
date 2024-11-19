5278b7c5c46cddebec2dc9631f54d11506ae3361
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-20 00:17:04 +0200

    mmh

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import generics.ListAPIView
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
