01559c95a9a9f782b2b741c226406a2e4661e942
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-20 00:56:27 +0200

    viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import generics.ListAPIView
 from rest_framework.viewsets import BookViewSet
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets.ModelViewSet
 
 # Create your views here.
 class BookList(generics.ListAPIView):
