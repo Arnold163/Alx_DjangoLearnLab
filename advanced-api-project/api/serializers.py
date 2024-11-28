390fd0f1754f2623878a2583351a3af78aabeb43
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-11-28 16:49:11 +0200

    nested serials

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,31 +2,25 @@ from rest_framework import serializers
 from .models import Author, Book
 from datetime import datetime
 
-
-class AuthorSerializer(serializers.ModelSerializer):
-    """serializer for the book"""
-    class Meta:
-        model = Author
-        fields = ['id', 'title', 'publication_year', 'author']
-
-    def validate_publication_year(self, value):
-        """pub year not in the future"""
-        current_year = datetime.now().year
-        if value > current_year:
-            raise serializers.ValidationError("Publication year cannot be in the future.")
-        
-class AuthorSerializer(serializers.ModelSerializer):
-    """serial for the author model nested BookSerializers"""
-    books = BookSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Author
-        fields = ['id', 'name', 'books']
-
 class Bookserializer(serializers.ModelSerializer):
     class Meta:
         model = Book 
         fields = ['id', 'title', 'publication_year', 'author']
-        
+
+    def validate_publication_year(self, value):
+        """validate the publication year not in future"""
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+    
+class AuthorSerializer(serializers.ModelSerializer):
+    """serializer for the Author model with nested BookSerializer"""
+    books = Bookserializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'book']
+
     
 
