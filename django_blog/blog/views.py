e1a7d0beb79ac854482e58e596f95bd1c63c617e
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-03 23:41:25 +0200

    user auth

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+#custom viewes for registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def posts(request):
+    return render(request, 'blog/base.html')
+
+#private view
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+
