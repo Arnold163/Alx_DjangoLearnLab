96ef43d802456dee7e6c9f84fce45895c0fa6b10
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-04 09:19:24 +0200

    -s

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +10,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('posts/', views.posts, name='posts'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
