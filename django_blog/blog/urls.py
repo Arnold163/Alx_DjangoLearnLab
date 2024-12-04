75172028d2323d6ba80d1e09f090e4e6f5d798ea
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-04 10:57:23 +0200

    -s on comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import (
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView
+)
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -15,4 +21,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('posts/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment_new'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+
 ]
