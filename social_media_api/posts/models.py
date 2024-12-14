8d044ae560f0bf8251695dc0a84007d36acf1f37
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-14 19:35:29 +0200

    like n user

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
+from .models import Post
 
 # Create your models here.
-from django.db import models
-from django.conf import settings
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
@@ -14,12 +15,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevent duplicate likes
 
     def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
+        return f"{self.user.username} likes {self.post.title}"
