59836b077b69b7ba78d6cbffc92e0f998782ae07
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-14 16:18:07 +0200

    feedview

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
@@ -25,3 +27,20 @@ class PostViewSet(viewsets.ModelViewSet):
     ...
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
+
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Get the list of users the current user is following
+        following_users = request.user.following.all()
+        
+        # Filter posts by authors in the following list and order by creation date
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        
+        # Serialize the posts (assuming a PostSerializer is implemented)
+        from .serializers import PostSerializer
+        serialized_posts = PostSerializer(posts, many=True)
+        
+        return Response(serialized_posts.data)
