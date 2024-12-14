9199530f46a1907152eb544564953030ef243031
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-14 19:48:10 +0200

    like

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserFeedView
+from .views import UserFeedView, LikePostView, UnlikePostView
 
 urlpatterns = [
     path('feed/', UserFeedView.as_view(), name='user_feed'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like_post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike_post'),
 ]
