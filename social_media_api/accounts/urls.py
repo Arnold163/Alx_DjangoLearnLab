2651b1f21f144395413ba4c50cbc17a6a43aad99
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-14 16:21:44 +0200

    url follow

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView
+from .views import FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('profile/', UserLoginView.as_view(), name='user-profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
+    
 ]
